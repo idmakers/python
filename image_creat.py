8aa0451cdd992c3e6d2576279a828de65258f876
unknown <idmakers@gmail.com>
2019-05-28 19:26:45 +0800

    vedio AND image creat

--- image_creat.py
+++ image_creat.py
@@ -5,24 +5,48 @@ from tkinter import filedialog
 import math
 
 
- 
- 
-print("456" )
+
+def savedata (data,name):
+    with open(name, 'w') as outfile:
+    # I'm writing a header here just for the sake of readability
+    # Any line starting with "#" will be ignored by numpy.loadtxt
+        outfile.write('# Array shape: {0}\n'.format(data.shape))
+
+        # Iterating through a ndimensional array produces slices along
+        # the last axis. This is equivalent to data[i,:,:] in this case
+        for data_slice in data:
+
+            # The formatting string indicates that I'm writing out
+            # the values in left-justified columns 7 characters in width
+            # with 2 decimal places.  
+            np.savetxt(outfile, data_slice, fmt='%3d')
+
+        # Writing out a break to indicate different slices...
+            outfile.write('# New slice\n')
+
 def create_image3():
     img3 = cv.imread("E:\\D4wyR0JXkAAg1O-.jpg")
     newx,newy = math.floor(img3.shape[1]/4),math.floor(img3.shape[0]/4) #new size (w,h)
     newimage = cv.resize(img3,(newx,newy))
-    x = int(newx/4)
-    newimage[:, :, 0] = newimage[:, :, 0]
-    newimage[:, :, 1] = newimage[:, :, 1]
-    newimage[:, :, 2] = newimage[:, :, 2]
-    cv.imshow("test",newimage)
-    newimage = cv.cvtColor(newimage, cv.COLOR_BGR2HLS)
+    x = int(newx/2)
+    for i in range (0,x*2,1):
+        newimage[:, i, 0] = newimage[:, i, 0]*0
+        newimage[:, :, 1] = newimage[:, :, 1]
+        newimage[:, :, 2] = newimage[:, :, 2]
+    print(newimage[:, :,:])
+    #np.savetxt("filename1.txt", newimage[:, :,:],newline='\n',fmt='%3d')
+    savedata (newimage,"test1.txt")
     cv.imshow("test1",newimage)
-    newimage[:, :, 0] = newimage[:, :, 0]*0
-    newimage[:, :, 1] = newimage[:, :, 1]
-    newimage[:, :, 2] = newimage[:, :, 2]
-    cv.imshow("test2",newimage)
+    print("**********************************************" )
+    newimage = cv.cvtColor(newimage, cv.COLOR_BGR2HLS)
+    for i in range (0,x,1):
+        newimage[:, i, 0] = newimage[:, i, 0]
+        newimage[:, :, 1] = newimage[:, :, 1]
+        newimage[:, :, 2] = newimage[:, :, 2]
+    print(newimage[:, :, :])
+    #np.savetxt("filename3.txt", newimage[:, :,:],newline='\n',fmt='%3d')
+    savedata (newimage,"test2.txt")
+    cv.imshow("test",newimage)
    
 
     #img.reshape = (200,200
--- video.py
+++ video.py
@@ -1,34 +1,67 @@
 import numpy as np
 import cv2
 import math
+ 
+cap = cv2.VideoCapture("C:/Users/idmakers/Desktop/123.mkv")
 
-cap = cv2.VideoCapture(0)
-#faceCascade = cv2.CascadeClassifier('C:\opencv-build\install\etc\haarcascades\haarcascade_frontalface_default.xml')
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+
+
+def savedata (data,name):
+    with open(name, 'w') as outfile:
+    # I'm writing a header here just for the sake of readability
+    # Any line starting with "#" will be ignored by numpy.loadtxt
+        outfile.write('# Array shape: {0}\n'.format(data.shape))
+
+        # Iterating through a ndimensional array produces slices along
+        # the last axis. This is equivalent to data[i,:,:] in this case
+        for data_slice in data:
+
+            # The formatting string indicates that I'm writing out
+            # the values in left-justified columns 7 characters in width
+            # with 2 decimal places.  
+            np.savetxt(outfile, data_slice, fmt='%3d')
+
+        # Writing out a break to indicate different slices...
+            outfile.write('# New slice\n')
+
+
+# 关闭文件
+
+count = 0
 while(cap.isOpened()):
     ret, frame = cap.read()
+    if ret==True:
+        height , width , layers =  frame.shape
+        new_h=math.floor(height/2)
+        new_w=math.floor(width/4)
+        frame  = cv2.resize(frame, (new_w*2, new_h))
+        frameN  = cv2.resize(frame, (new_w, new_h))
+        #np.savetxt("filename1", frame[:, :, 0])
+        #for i in range (0,new_w,1):
+        frame[:, :, 0] = frame[:, :, 0]
+        #for i in range (0,new_w,4):
+        frame[:, :, 1] = frame[:, :, 1]
+        #for i in range (0,new_w,6):
+        frame[:, :, 2] = frame[:, :, 2]
+        #np.savetxt("filename", frame[:, :, 0])
+        cv2.imshow("frame",frameN)
+        c = str(count)
+        savedata(frame,"filename"+c+".txt")
+        count= count+1
+        
 
 
+    # write the flipped frame
+        
 
-    newx,newy = math.floor(frame.shape[1]/2),math.floor(frame.shape[0]/2) #new size (w,h)
-    newimage = cv2.resize(frame ,(newx,newy))
-    gray = cv2.cvtColor(newimage, cv2.COLOR_BGR2GRAY)
-    HSV =cv2.cvtColor(newimage, cv2.COLOR_BGR2HSV)
-    RGB =cv2.cvtColor(newimage, cv2.COLOR_BGR2RGB)
-    YCrCb =cv2.cvtColor(newimage, cv2.COLOR_BGR2YCrCb)
-    XYZ =cv2.cvtColor(newimage, cv2.COLOR_BGR2XYZ)
-    HLS =cv2.cvtColor(newimage, cv2.COLOR_BGR2HLS)
-    CIE =cv2.cvtColor(newimage, cv2.COLOR_BGR2Lab)
-    cv2.imshow("bgr2gray",gray)
-    cv2.imshow("bgr2HSV",HSV)
-    cv2.imshow("bgr2RGB",RGB)
-    cv2.imshow("BGR2YCbCr",YCrCb)
-    cv2.imshow("BGR2XYZ",XYZ)
-    cv2.imshow("BGR2HLS",HLS)
-    cv2.imshow("BGR2CIE",CIE)
-    #cv2.imshow("image",frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+        #cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
         break
-
+# Release everything if job is finished
 cap.release()
 cv2.destroyAllWindows()
